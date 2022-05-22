2c673362ee6b853bf5ff3a825cf0469942564a27
Radish <53844660+radixsh@users.noreply.github.com>
2022-05-21 21:09:24 -0700

    Add disabled selected option, write hexToAscii

--- convert.js
+++ convert.js
@@ -1,28 +1,29 @@
 document.addEventListener("DOMContentLoaded", function() {
     document.getElementById("base").onchange = function () {
-        var base = document.getElementById("base").value;
-        var input = document.getElementById("input").value;
-        if (base == "ascii") {
+        var base = document.getElementById("base");
+        var input = document.getElementById("input");
+        text = input.value;
+        if (base.value == "ascii") {
             var ascii_output_element = document.getElementById("ascii");
-            ascii_output_element.innerHTML = "[Irrelevant]" 
+            ascii_output_element.innerHTML = "[Irrelevant]"
             var hex_output_element = document.getElementById("hex");
-            hex_output_element.innerHTML = asciiToHex(input);
+            hex_output_element.innerHTML = asciiToHex(text);
             var bin_output_element = document.getElementById("bin");
-            bin_output_element.innerHTML = convert(asciiToHex(input), 16, 2);
-        } else if (base == "hex") {
+            bin_output_element.innerHTML = convert(asciiToHex(text), 16, 2);
+        } else if (base.value == "hex") {
             var ascii_output_element = document.getElementById("ascii");
-            ascii_output_element.innerHTML = convert(asciiToHex(input), 16, 2);
+            ascii_output_element.innerHTML = hexToAscii(text);
             var hex_output_element = document.getElementById("hex");
-            hex_output_element.innerHTML = "[Irrelevant]" 
+            hex_output_element.innerHTML = "[Irrelevant]"
             var bin_output_element = document.getElementById("bin");
-            bin_output_element.innerHTML = convert(input, 16, 2);
-        } else if (base == "bin") {
+            bin_output_element.innerHTML = convert(text, 16, 2);
+        } else if (base.value == "bin") {
             var ascii_output_element = document.getElementById("ascii");
-            ascii_output_element.innerHTML = convert(asciiToHex(input), 16, 2);
+            ascii_output_element.innerHTML = hexToAscii(parseInt(text, 2));
             var hex_output_element = document.getElementById("hex");
-            hex_output_element.innerHTML = convert(input, 2, 16);
+            hex_output_element.innerHTML = convert(text, 2, 16);
             var bin_output_element = document.getElementById("bin");
-            bin_output_element.innerHTML = "[Irrelevant]" 
+            bin_output_element.innerHTML = "[Irrelevant]"
         }
     }
 });
@@ -31,7 +32,13 @@ function convert(number, original_base, new_base) {
     return parseInt(number, original_base).toString(new_base);
 }
 
-function asciiToHex(ascii, hex_output_element) {
+function hexToAscii(hex_string) {
+    // split hex string into pairs: https://stackoverflow.com/questions/55549405/split-string-every-2-character-into-array
+    var pairs = hex_string.match(/.{1,2}/g); 
+    return hex_string.fromCharCode(pairs);
+}
+
+function asciiToHex(ascii) {
     var hex_values = [];
     for (var n = 0, l = ascii.length; n < l; n++) {
         var hex = Number(ascii.charCodeAt(n)).toString(16);
